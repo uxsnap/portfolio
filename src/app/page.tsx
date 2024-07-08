2b88a24b144c4932426e34f795b54ce61e5e6e9b
uxsnap <nuxxx7132@gmail.com>
2024-07-08 22:27:47 +0200

    add me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
@@ -6,6 +8,10 @@ export default function Home() {
       <div className={styles.title}>
         <h1>Software</h1>
         <h2>Engineer</h2>
+      </div>
+
+      <div className={styles.me}>
+        <img src="/static/me.png" alt="_me" />
       </div>
     </div>
   );
